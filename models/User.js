8195752b09cbaee98a54372fb0545c395c7478a5
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-01-25 18:49:20 +0500

    Structure

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema();
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const UserSchema= Schema({
+email:string,
+salt:string,
+hash:string,
+image:string,
+});
+
+UserSchema.methods.validPassword = function() {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+}
+
+UserSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+}
+
+module.exports = mongoose.model("User",UserSchema);
